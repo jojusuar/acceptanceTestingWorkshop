910876f65da3cb20f02c85b998f120ee736f4f5a
hailie <142462676+se0klie@users.noreply.github.com>
2025-12-11 15:43:13 -0500

    list tasks function

--- todo_list.py
+++ todo_list.py
@@ -1,6 +1,12 @@
 tasklist = {}
 
+
 def add_task(task: str):
     tasklist[task] = False
 
+
+def all_tasks():
+    for i, (task, done) in enumerate(tasklist.items()):
+        print(f"Task {i}: {task} -> Completed: {done}")
+        
 # implementar lcada requerimiento como función
